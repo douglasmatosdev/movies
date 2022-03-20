931a1c1f4b82c4af24c502766d0c70981020b373
douglasmatosdev <douglasmatosdev@gmail.com>
2022-03-20 00:36:54 -0300

    Fix scores PUT no work

--- backend/src/main/java/com/douglasmatosdev/movie/controllers/ScoreController.java
+++ backend/src/main/java/com/douglasmatosdev/movie/controllers/ScoreController.java
@@ -9,7 +9,6 @@ import org.springframework.web.bind.annotation.*;
 @RestController
 @RequestMapping(value = "/scores")
 public class ScoreController {
-
     @Autowired
     private ScoreService service;
 
