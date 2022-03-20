33e3df61e5d4023514543ddefaeda404d9bfc2a4
douglasmatosdev <douglasmatosdev@gmail.com>
2022-03-20 00:30:06 -0300

    /scores not work in prod

--- backend/src/main/java/com/douglasmatosdev/movie/controllers/ScoreController.java
+++ backend/src/main/java/com/douglasmatosdev/movie/controllers/ScoreController.java
@@ -2,11 +2,8 @@ package com.douglasmatosdev.movie.controllers;
 
 import com.douglasmatosdev.movie.dto.MovieDTO;
 import com.douglasmatosdev.movie.dto.ScoreDTO;
-import com.douglasmatosdev.movie.services.MovieService;
 import com.douglasmatosdev.movie.services.ScoreService;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.data.domain.Page;
-import org.springframework.data.domain.Pageable;
 import org.springframework.web.bind.annotation.*;
 
 @RestController
